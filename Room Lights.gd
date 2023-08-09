a67d004959d62adee5d5f2e7722c0f096718ff28
Liam (PC) <your.email@example.com>
2023-08-09 17:25:14 +1200

    Ready for test

--- Room Lights.gd
+++ Room Lights.gd
@@ -4,7 +4,7 @@ extends Node3D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for n in self.get_children():
-		n.light_energy = 0.02
+		n.light_energy = 0.01
 #		n.light_energy = 1
 
 
--- pattern_controll.gd
+++ pattern_controll.gd
@@ -40,9 +40,10 @@ const HEIGHT = 500
 
 const MIN_DB = 60
 
-const debug_color = Color.DEEP_SKY_BLUE
+const debug_color = Color.WHITE
 
 var socket = WebSocketPeer.new()
+var light_energy = 0
 # Called when the node enters the scene tree for the first time.
 
 
@@ -50,6 +51,7 @@ func _ready():
 	spectrum = AudioServer.get_bus_effect_instance(0,0)
 	current_pattern = patterns.small[0]
 	print(socket.connect_to_url("localhost:8888"))
+	get_parent().get_node("OmniLight3D").light_color = debug_color
 #	for N in get_node("Top Down").get_children():
 #		N.light_energy = 0
 #		N.light_color = Color.CORNFLOWER_BLUE
@@ -65,21 +67,24 @@ func _process(delta):
 			var magnitude: float = spectrum.get_magnitude_for_frequency_range(cfr[0], cfr[1]).length()
 			var energy = clamp((MIN_DB + linear_to_db(magnitude)) / MIN_DB, 0, 1)
 			var height = energy * HEIGHT
-			var light_colour = max(remap_range(height, 0, 500, 0, 2),remap_range(set.animation.min, 0, 255, 0, 2))
+#			var light_energy = max(remap_range(height, 0, 500, 0, 2),remap_range(set.animation.min, 0, 255, 0, 2))
 			if height > 300:
 				print("0|"+str(int(remap_range(height, 0, 500, 0, 255))))
 				socket.send_text("0|255")
-				light_colour = 5
+				light_energy = 5
 			else:
 				socket.send_text("0|0")
+				light_energy = 0
+			socket.send_text("10|"+str(int(remap_range(height, 0, 500, 0, 255))))
+			get_parent().get_node("OmniLight3D").light_energy = remap_range(light_energy,0,2,0,0.3)
 			if set.lights == "_all":
 				for N in self.get_children():
 					for n in N.get_children():
-						print(light_colour)
-						n.light_energy = light_colour
+						print(light_energy)
+						n.light_energy = light_energy
 						n.light_color = debug_color
-					
-						
+
+
 #			for N in get_node("Top Down").get_children():
 #				print(N)
 #				N.light_energy = height / 200
